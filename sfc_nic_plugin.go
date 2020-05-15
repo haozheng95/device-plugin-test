79faf19925dd2c36d02f221c4a238370378f9c92
tesla7 <77625961@qq.com>
2020-05-15 16:46:41 +0800

    log

--- sfc_nic_plugin.go
+++ sfc_nic_plugin.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"os"
-	"path"
-	"strings"
-
 	//"strconv"
 	"bytes"
 	"os/exec"
@@ -12,7 +9,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/golang/glog"
+	"io/ioutil"
 	"net"
+	"path"
+	"regexp"
+	"strings"
 	"sync"
 	"time"
 
@@ -34,21 +35,15 @@ var nodeLabelVersion string
 //)
 // sfcNICManager manages Solarflare NIC devices
 type sfcNICManager struct {
-	devices map[string]*pluginapi.Device
+	devices     map[string]*pluginapi.Device
+	deviceFiles []string
 }
 
 func NewSFCNICManager() (*sfcNICManager, error) {
-	//return &sfcNICManager{
-	//	devices:     make(map[string]*pluginapi.Device),
-	//}, nil
-
-	t := &sfcNICManager{
-		devices: make(map[string]*pluginapi.Device),
-	}
-
-	dev := pluginapi.Device{ID: "1", Health: pluginapi.Healthy}
-	t.devices["1"] = &dev
-	return t, nil
+	return &sfcNICManager{
+		devices:     make(map[string]*pluginapi.Device),
+		deviceFiles: []string{"/dev/onload", "/dev/onload_cplane", "dev/onload_epoll", "/dev/sfc_char", "/dev/sfc_affinity"},
+	}, nil
 }
 
 func ExecCommand(cmdName string, arg ...string) (bytes.Buffer, error) {
@@ -66,64 +61,64 @@ func ExecCommand(cmdName string, arg ...string) (bytes.Buffer, error) {
 	return out, err
 }
 
-//func (sfc *sfcNICManager) discoverSolarflareResources() bool {
-//	found := false
-//	sfc.devices = make(map[string]*pluginapi.Device)
-//	glog.Info("discoverSolarflareResources")
-//	out, err := ExecCommand("lshw", "-short", "-class", "network")
-//	if err != nil {
-//		glog.Errorf("Error while discovering: %v", err)
-//		return found
-//	}
-//	re := regexp.MustCompile(regExpSFC)
-//	sfcNICs := re.FindAllString(out.String(), -1)
-//	for _, nic := range sfcNICs {
-//		//nameIf := strings.Fields(nic)[1]
-//		fmt.Printf("logical name: %v \n", strings.Fields(nic)[1])
-//		//if nameIf == "network" {
-//		// When running inside non net=host container, 'devices' column is empty. 'class'
-//		// value, 'network', is being parsed as 'Device' incorrectly. Skip it.
-//		// H/W path             Device  Class          Description
-//		// =======================================================
-//		// /0/100/3/0.1                 network        Ethernet Controller XL710 for 40GbE QSFP+
-//		///0/3/0                        network        SFC9020 [Solarstorm]
-//		//	continue
-//		//}
-//		dev := pluginapi.Device{ID: strings.Fields(nic)[1], Health: pluginapi.Healthy}
-//		sfc.devices[strings.Fields(nic)[1]] = &dev
-//		found = true
-//	}
-//	fmt.Printf("Devices: %v \n", sfc.devices)
-//
-//	return found
-//}
+func (sfc *sfcNICManager) discoverSolarflareResources() bool {
+	found := false
+	sfc.devices = make(map[string]*pluginapi.Device)
+	glog.Info("discoverSolarflareResources")
+	out, err := ExecCommand("lshw", "-short", "-class", "network")
+	if err != nil {
+		glog.Errorf("Error while discovering: %v", err)
+		return found
+	}
+	re := regexp.MustCompile(regExpSFC)
+	sfcNICs := re.FindAllString(out.String(), -1)
+	for _, nic := range sfcNICs {
+		//nameIf := strings.Fields(nic)[1]
+		fmt.Printf("logical name: %v \n", strings.Fields(nic)[1])
+		//if nameIf == "network" {
+		// When running inside non net=host container, 'devices' column is empty. 'class'
+		// value, 'network', is being parsed as 'Device' incorrectly. Skip it.
+		// H/W path             Device  Class          Description
+		// =======================================================
+		// /0/100/3/0.1                 network        Ethernet Controller XL710 for 40GbE QSFP+
+		///0/3/0                        network        SFC9020 [Solarstorm]
+		//	continue
+		//}
+		dev := pluginapi.Device{ID: strings.Fields(nic)[1], Health: pluginapi.Healthy}
+		sfc.devices[strings.Fields(nic)[1]] = &dev
+		found = true
+	}
+	fmt.Printf("Devices: %v \n", sfc.devices)
 
-//func (sfc *sfcNICManager) isOnloadInstallHealthy() bool {
-//	healthy := false
-//	//cmdName := "onload"
-//	cmdName := "ssh"
-//	//out, _ := ExecCommand(cmdName, "--version")
-//	out, _ := ExecCommand(cmdName, "-o", "StrictHostKeyChecking=no", "127.0.0.1", "onload --version")
-//
-//	if strings.Contains(out.String(), "Solarflare Communications") && strings.Contains(out.String(), onloadver) {
-//		//cmdName = "/sbin/ldconfig"
-//		out, _ := ExecCommand(cmdName, "-o", "StrictHostKeyChecking=no", "127.0.0.1", "/sbin/ldconfig -N -v")
-//
-//		if strings.Contains(out.String(), "libonload") {
-//			if AreAllOnloadDevicesAvailable() == true {
-//				fmt.Println("All Onload devices Verified\n")
-//				healthy = true
-//			} else {
-//				fmt.Errorf("Inconsistent Onload installation. All Onload devices are not available!!!")
-//			}
-//		} else {
-//			fmt.Errorf("Inconsistent Onload installation. libonload not detected.")
-//		}
-//	} else {
-//		fmt.Errorf("Inconsistent Onload installation.")
-//	}
-//	return healthy
-//}
+	return found
+}
+
+func (sfc *sfcNICManager) isOnloadInstallHealthy() bool {
+	healthy := false
+	//cmdName := "onload"
+	cmdName := "ssh"
+	//out, _ := ExecCommand(cmdName, "--version")
+	out, _ := ExecCommand(cmdName, "-o", "StrictHostKeyChecking=no", "127.0.0.1", "onload --version")
+
+	if strings.Contains(out.String(), "Solarflare Communications") && strings.Contains(out.String(), onloadver) {
+		//cmdName = "/sbin/ldconfig"
+		out, _ := ExecCommand(cmdName, "-o", "StrictHostKeyChecking=no", "127.0.0.1", "/sbin/ldconfig -N -v")
+
+		if strings.Contains(out.String(), "libonload") {
+			if AreAllOnloadDevicesAvailable() == true {
+				fmt.Println("All Onload devices Verified\n")
+				healthy = true
+			} else {
+				fmt.Errorf("Inconsistent Onload installation. All Onload devices are not available!!!")
+			}
+		} else {
+			fmt.Errorf("Inconsistent Onload installation. libonload not detected.")
+		}
+	} else {
+		fmt.Errorf("Inconsistent Onload installation.")
+	}
+	return healthy
+}
 
 func (sfc *sfcNICManager) installOnload() error {
 	cmdName := "yum"
@@ -186,9 +181,9 @@ func (sfc *sfcNICManager) installOnload() error {
 			//fmt.Println("CMD--" + cmdName + ": " + out.String())
 			cmdName = "onload_tool reload"
 			_, err = ExecCommand("onload_tool", "reload")
-			//if !sfc.isOnloadInstallHealthy() {
-			//	return fmt.Errorf("Onload Install Failed!!")
-			//}
+			if !sfc.isOnloadInstallHealthy() {
+				return fmt.Errorf("Onload Install Failed!!")
+			}
 		} else {
 			return fmt.Errorf("onload_install could not be completed!!!!")
 		}
@@ -200,8 +195,8 @@ func (sfc *sfcNICManager) installOnload() error {
 
 func (sfc *sfcNICManager) Init() error {
 	glog.Info("Init\n")
-	//err := sfc.installOnload()
-	return nil
+	err := sfc.installOnload()
+	return err
 }
 
 func Register(kubeletEndpoint string, pluginEndpoint, socketName string) error {
@@ -219,11 +214,10 @@ func Register(kubeletEndpoint string, pluginEndpoint, socketName string) error {
 		Endpoint:     pluginEndpoint,
 		ResourceName: resourceName,
 	}
-	fmt.Println(reqt)
 
 	_, err = client.Register(context.Background(), reqt)
 	if err != nil {
-		return fmt.Errorf("device-plugin: cannot register to kubelet service: %v \n", err)
+		return fmt.Errorf("device-plugin: cannot register to kubelet service: %v", err)
 	}
 	return nil
 }
@@ -232,23 +226,22 @@ func Register(kubeletEndpoint string, pluginEndpoint, socketName string) error {
 func (sfc *sfcNICManager) ListAndWatch(emtpy *pluginapi.Empty, stream pluginapi.DevicePlugin_ListAndWatchServer) error {
 	glog.Info("device-plugin: ListAndWatch start\n")
 	for {
-		//sfc.discoverSolarflareResources()
-		//if !sfc.isOnloadInstallHealthy() {
-		//	glog.Errorf("Error with onload installation. Marking devices unhealthy.")
-		//	for _, device := range sfc.devices {
-		//		device.Health = pluginapi.Unhealthy
-		//	}
-		//}
-		//resp := new(pluginapi.ListAndWatchResponse)
-		//for _, dev := range sfc.devices {
-		//	glog.Info("dev ", dev)
-		//	resp.Devices = append(resp.Devices, dev)
-		//}
-		//glog.Info("resp.Devices ", resp.Devices)
-		//if err := stream.Send(resp); err != nil {
-		//	glog.Errorf("Failed to send response to kubelet: %v\n", err)
-		//}
-		glog.Info("device-plugin: ListAndWatch watch........\n")
+		sfc.discoverSolarflareResources()
+		if !sfc.isOnloadInstallHealthy() {
+			glog.Errorf("Error with onload installation. Marking devices unhealthy.")
+			for _, device := range sfc.devices {
+				device.Health = pluginapi.Unhealthy
+			}
+		}
+		resp := new(pluginapi.ListAndWatchResponse)
+		for _, dev := range sfc.devices {
+			glog.Info("dev ", dev)
+			resp.Devices = append(resp.Devices, dev)
+		}
+		glog.Info("resp.Devices ", resp.Devices)
+		if err := stream.Send(resp); err != nil {
+			glog.Errorf("Failed to send response to kubelet: %v\n", err)
+		}
 		time.Sleep(5 * time.Second)
 	}
 	return nil
@@ -260,16 +253,16 @@ func (sfc *sfcNICManager) Allocate(ctx context.Context, rqt *pluginapi.AllocateR
 	//	containerName := strings.Join([]string{"k8s", "POD", rqt.PodName, rqt.Namespace}, "_")
 	for _, id := range rqt.DevicesIDs {
 		if _, ok := sfc.devices[id]; ok {
-			//for _, d := range sfc.deviceFiles {
-			//	resp.Devices = append(resp.Devices, &pluginapi.DeviceSpec{
-			//		HostPath:      d,
-			//		ContainerPath: d,
-			//		Permissions:   "mrw",
-			//	})
-			//}
+			for _, d := range sfc.deviceFiles {
+				resp.Devices = append(resp.Devices, &pluginapi.DeviceSpec{
+					HostPath:      d,
+					ContainerPath: d,
+					Permissions:   "mrw",
+				})
+			}
 			glog.Info("Allocated interface ", id)
 			//glog.Info("Allocate interface ", id, " to ", containerName)
-			//go MoveInterface(id)
+			go MoveInterface(id)
 		}
 	}
 	return resp, nil
@@ -277,55 +270,53 @@ func (sfc *sfcNICManager) Allocate(ctx context.Context, rqt *pluginapi.AllocateR
 
 func MoveInterface(interfaceName string) {
 	glog.Info("move interface after reading checkpoint file")
-	return
-	//_, err := ExecCommand("/usr/bin/cont-sfc-nic-move.sh", interfaceName, k8sAPI)
-	//if err != nil {
-	//	glog.Error(err)
-	//}
+	_, err := ExecCommand("/usr/bin/cont-sfc-nic-move.sh", interfaceName, k8sAPI)
+	if err != nil {
+		glog.Error(err)
+	}
 }
 
 func AnnotateNodeWithOnloadVersion(version string) {
 	glog.Info("Annotating Node with onload version: ", version, " ", nodeLabelVersion)
-	return
-	////TODO: Read api url from config map
-	//out, err := ExecCommand("/usr/bin/annotate_node.sh", k8sAPI, nodeLabelVersion, version)
-	//if err != nil {
-	//	glog.Error(err)
-	//}
-	//glog.Info(out.String())
+	//TODO: Read api url from config map
+	out, err := ExecCommand("/usr/bin/annotate_node.sh", k8sAPI, nodeLabelVersion, version)
+	if err != nil {
+		glog.Error(err)
+	}
+	glog.Info(out.String())
 }
 
-//func AreAllOnloadDevicesAvailable() bool {
-//	glog.Info("AreAllOnloadDevicesAvailable\n")
-//
-//	found := 0
-//
-//	// read the whole file at once
-//	b, err := ioutil.ReadFile("/gopath/proc/devices")
-//	if err != nil {
-//		panic(err)
-//	}
-//	s := string(b)
-//
-//	if strings.Index(s, "onload_epoll") > 0 {
-//		found++
-//	}
-//
-//	if strings.Index(s, "onload_cplane") > 0 {
-//		found++
-//	}
-//
-//	// '\n' is added to avoid a match with onload_cplane and onload_epoll
-//	if strings.Index(s, "onload\n") > 0 {
-//		found++
-//	}
-//
-//	if found == 3 {
-//		return true
-//	} else {
-//		return false
-//	}
-//}
+func AreAllOnloadDevicesAvailable() bool {
+	glog.Info("AreAllOnloadDevicesAvailable\n")
+
+	found := 0
+
+	// read the whole file at once
+	b, err := ioutil.ReadFile("/gopath/proc/devices")
+	if err != nil {
+		panic(err)
+	}
+	s := string(b)
+
+	if strings.Index(s, "onload_epoll") > 0 {
+		found++
+	}
+
+	if strings.Index(s, "onload_cplane") > 0 {
+		found++
+	}
+
+	// '\n' is added to avoid a match with onload_cplane and onload_epoll
+	if strings.Index(s, "onload\n") > 0 {
+		found++
+	}
+
+	if found == 3 {
+		return true
+	} else {
+		return false
+	}
+}
 
 func (sfc *sfcNICManager) UnInit() {
 	var out bytes.Buffer
@@ -351,57 +342,48 @@ func main() {
 
 	onloadver = os.Args[1] //"201606-u1.3"
 	//onloadsrc = os.Args[2]    //"http://www.openonload.org/download/openonload-" + onloadver + ".tgz"
-	//regExpSFC = os.Args[2]    //"(?m)[\r\n]+^.*SFC[6-9].*$"
+	regExpSFC = os.Args[2]    //"(?m)[\r\n]+^.*SFC[6-9].*$"
 	socketName = os.Args[3]   //"sfcNIC"
 	resourceName = os.Args[4] //"pod.alpha.kubernetes.io/opaque-int-resource-sfcNIC"
 	k8sAPI = os.Args[5]
 	nodeLabelVersion = os.Args[6]
 	flag.Lookup("logtostderr").Value.Set("true")
-	fmt.Printf("main================1111111 \n")
-	fmt.Printf("onloadver=%s \n", onloadver)
-	//fmt.Printf("regExpSFC=%s \n", regExpSFC)
-	fmt.Printf("socketName=%s \n", socketName)
 
-	fmt.Printf("resourceName=%s \n", resourceName)
-	fmt.Printf("k8sAPI=%s \n", k8sAPI)
-	fmt.Printf("nodeLabelVersion=%s \n", nodeLabelVersion)
 	sfc, err := NewSFCNICManager()
 	if err != nil {
 		glog.Fatal(err)
 		os.Exit(1)
 	}
 
-	//found := sfc.discoverSolarflareResources()
-	//if !found {
-	//	// clean up any exisiting device plugin software
-	//	//sfc.UnInit()
-	//	glog.Errorf("No SolarFlare NICs are present\n")
-	//	os.Exit(1)
-	//}
-	//if !sfc.isOnloadInstallHealthy() {
-	//	//err = sfc.Init()
-	//	//if err != nil {
-	//	glog.Errorf("Error with onload installation")
-	//	//		for _, device := range sfc.devices {
-	//	//			device.Health = pluginapi.Unhealthy
-	//	//		}
-	//	//	}
-	//	AnnotateNodeWithOnloadVersion("")
-	//}
-	//AnnotateNodeWithOnloadVersion(onloadver)
+	found := sfc.discoverSolarflareResources()
+	if !found {
+		// clean up any exisiting device plugin software
+		//sfc.UnInit()
+		glog.Errorf("No SolarFlare NICs are present\n")
+		os.Exit(1)
+	}
+	if !sfc.isOnloadInstallHealthy() {
+		//err = sfc.Init()
+		//if err != nil {
+		glog.Errorf("Error with onload installation")
+		//		for _, device := range sfc.devices {
+		//			device.Health = pluginapi.Unhealthy
+		//		}
+		//	}
+		AnnotateNodeWithOnloadVersion("")
+	}
+	AnnotateNodeWithOnloadVersion(onloadver)
 
 	pluginEndpoint := fmt.Sprintf("%s-%d.sock", socketName, time.Now().Unix())
-
+	//serverStarted := make(chan bool)
 	var wg sync.WaitGroup
 	wg.Add(1)
 	// Starts device plugin service.
-	fmt.Printf("main================2222222 \n")
 	go func() {
 		defer wg.Done()
 		fmt.Printf("DveicePluginPath %s, pluginEndpoint %s\n", pluginapi.DevicePluginPath, pluginEndpoint)
 		fmt.Printf("device-plugin start server at: %s\n", path.Join(pluginapi.DevicePluginPath, pluginEndpoint))
 		lis, err := net.Listen("unix", path.Join(pluginapi.DevicePluginPath, pluginEndpoint))
-		//lis, err := net.Listen("tcp", "127。0。0。1:80")
 		if err != nil {
 			glog.Fatal(err)
 			return
@@ -414,10 +396,7 @@ func main() {
 	// TODO: fix this
 	time.Sleep(5 * time.Second)
 	// Registers with Kubelet.
-	fmt.Printf("Registers with Kubelet=====%s\n", "SSSSSSSS")
-	fmt.Printf("pluginEndpoint====%s\n", pluginEndpoint)
-	fmt.Printf("resourceName=====%s\n", resourceName)
-	err = Register(pluginapi.KubeletSocket, path.Join(pluginapi.DevicePluginPath, pluginEndpoint), resourceName)
+	err = Register(pluginapi.KubeletSocket, pluginEndpoint, resourceName)
 	if err != nil {
 		glog.Fatal(err)
 	}
